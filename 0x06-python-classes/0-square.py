8a31bc910270ea3381c7197baeee68ba548c4485
Al-Serra <mathematic104@gmail.com>
2023-12-23 07:07:46 +0530

    yalaaaaaaaaaaaa

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module 0-square.py.
+
+This module has only one class.
+
+Classes:
+    Square:
+        A class representing a Square.
+
+Functions:
+    This module has no functions.
+"""
+
+
+class Square:
+    """
+    A class representing a Square.
+
+    This is an empty class.
+    """
+
+    pass
